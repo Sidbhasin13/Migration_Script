d25fd84d603432e8b79532e71629d026ba57627b
Sidbhasin13 <110635179+Sidbhasin13@users.noreply.github.com>
2024-09-29 21:41:10 +0530

    Update script.py

--- script.py
+++ script.py
@@ -10,33 +10,11 @@ import argparse
 from requests.auth import HTTPBasicAuth
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# Github Token
-# github_token_migration = 'Bearer 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'
-
-# ArgParser Section
-# parser = argparse.ArgumentParser()
-# parser.add_argument("--user", "-u1", help="User Name")
-# parser.add_argument("--password", "-p1", help="Password")
-# parser.add_argument("--Github_Repo_Name", "-gb", help="Github Repository Name")
-# parser.add_argument("--Gitlab_Project_Id", "-gl", help="Gitlab Project ID")
-# args = parser.parse_args()
-
-
-# if args.user:
-#     gitlab_user_name = args.user
-# if args.password:
-#     gitlab_user_pass = args.password
-# if args.Github_Repo_Name:
-#     github_repo_name = args.Github_Repo_Name
-# if args.Gitlab_Project_Id:
-#     gl_project_id = args.Gitlab_Project_Id
-
-
 
 gitlab_user_name = "Sidbhasin13"
 print("***** Arg User *****", gitlab_user_name)
 
-gitlab_user_pass = "Sidbhasinnewdelhi@0786"
+gitlab_user_pass = "gitlab_password"
 print("***** Arg Password *****", gitlab_user_pass)
 
 github_repo_name = "Migration_Script"
@@ -71,13 +49,13 @@ print('***** ????? *****', repoDirectory)
 def list_repos():
     repos = []
     page = 1
-#     headers = {'Authorization': 'Bearer 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', 'Cookie': '_octo=GH1.1.784827204.1673029009; logged_in=no'}
+#     headers = {'Authorization': 'Bearer Token', 'Cookie': '_octo=GH1.1.784827204.1673029009; logged_in=no'}
     while True:
-#         params = {"page": page, "per_page": 10, headers = {'Authorization': 'Bearer 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'}}
+#         params = {"page": page, "per_page": 10, headers = {'Authorization': 'Bearer Token'}}
 #         print("----- Params -----", params)
         url = 'https://api.github.com/users/Sidbhasin13/repos'
         print("---- URL ----", url)
-        res = requests.request("GET", url, auth=HTTPBasicAuth('Sidbhasin13', '@$ID&bHA786'), verify=False)
+        res = requests.request("GET", url, auth=HTTPBasicAuth('Sidbhasin13', 'Password'), verify=False)
         print("+++++ Response +++++", res)
 #         repos += res.json()
         repos.extend(i['full_name'] for i in res.json())
